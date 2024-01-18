ab1313d9567de5c7ef0435f27f47ed900da1acdc
Oki1 <oskar.rotar@gmail.com>
2024-01-18 20:30:52 +0100

    validation layers should be working in debug mode

--- vulkano.cpp
+++ vulkano.cpp
@@ -5,48 +5,90 @@
 #include <iostream>
 #include <vector>
 
-int main(void) {
-    //setup glfw window
-    std::string name("Vulkan uwu");
-    const unsigned int WIDTH = 1920;
-    const unsigned int HEIGHT = 1080;
-    glfwInit();
+const unsigned int WIDTH = 1920;
+const unsigned int HEIGHT = 1080;
 
-    //setup validation layers for debugging
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = true;
+    printf("Debug mode activated");
     const std::vector<const char*> validationLayers = {
         "VK_LAYER_KHRONOS_validation"
     };
-    #ifdef NDEBUG
-        const bool enableValidationLayers = false;
-    #else
-        std::cout << "Debug mode detected, validation layers enabled\n";
-        const bool enableValidationLayers = true;
-    #endif
+    
+    bool checkValidationLayerSupport() {
+        int layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : validationLayers) {
+            bool found = false;
+            for (const auto& layerProperties : availableLayers) {
+                if(strcmp(layerName, layerProperties.layerName) == 0) {
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                return false;
+            }
+        }
+        return true;
+    }
+#else 
+    const bool enableValidationLayers = false;
+
+    const std::vector<const char*> validationLayers = {};
+    bool checkValidationLayerSupport() { return false;}
+#endif
+
+int main(void) {
+    //setup glfw window
+    std::string name("Vulkan uwu");
+    glfwInit();
+
+    //setup validation layers for debugging
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, name.c_str(), nullptr, nullptr);
 
     //init vulkan
     VkInstance vulkanInstance;
-    VkApplicationInfo appInfo {
-        VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        nullptr,
-        name.c_str(),
-        VK_MAKE_VERSION(0,0,1),
-        "No engine",
-        VK_MAKE_VERSION(1,0,0),
-        VK_API_VERSION_1_0
-    };
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = name.c_str();
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    //appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    VkInstanceCreateInfo createInfo;
+    VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
+
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions;
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
     createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledLayerNames = glfwExtensions;
-    createInfo.enabledLayerCount = 0;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+
+    if(enableValidationLayers) {
+        if(!checkValidationLayerSupport()) {
+            std::cerr << "Validation layers requested, but not available" << std::endl;
+            return 1;
+        }
+        createInfo.enabledLayerCount = validationLayers.size();
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
+
+
+    
     if(vkCreateInstance(&createInfo, nullptr, &vulkanInstance) != VK_SUCCESS) {
         std::cerr << "Failed to create Vulkan instance!" << std::endl;
         return 1;
